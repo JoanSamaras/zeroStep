ba4767930c2373ac65d519a9b80c35418cc31967
Joan Samaras <jsam.art14@gmail.com>
2020-12-16 23:47:51 +0200

    main - update on the dependencies and addition of buttons and colours

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import styled from 'styled-components';
+import { colours } from 'design-system/colours';
 
-const Header = styled.div`
-    background-color: #282c34;
-    min-height: 100vh;
+const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
+    background-color: ${ colours.grey1 };
+    max-height: 100vh;
+    max-width: 100vw;
 `;
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
     return (
-        <div className="App">
-            <Header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </Header>
-        </div>
+        <Wrapper />
     );
-}
+};
 
 export default App;
---
+++ src/components/buttons.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { colours } from 'design-system/colours';
+import { spacings } from 'design-system/spacings';
+import { fontSizes } from 'design-system/font-sizes';
+import { fontWeights } from 'design-system/font-weights';
+import { Margins, margins } from 'helpers/css-utils';
+
+type ButtonProfileType = 'default' | 'primary'
+type ButtonSize = 'default' | 'small'
+
+type ButtonStyleProps = {
+    profile: ButtonProfileType
+    size: ButtonSize
+    disabled?: boolean
+    margins?: Margins
+}
+
+const profileColourSettings = ( p: ButtonProfileType ) => {
+    if ( p === 'primary' ) {
+        return css`
+            background-color: ${ colours.primary5 };
+            color: ${ colours.grey1 };
+
+            :hover {
+                cursor: pointer;
+                background-color: ${ colours.primary4 };
+            }
+            :active {
+                cursor: pointer;
+                background-color: ${ colours.primary6 };
+            }
+        `
+    } else {
+        return css`
+            background-color: ${ colours.grey3 };
+            color: ${ colours.grey4 };
+
+            :hover {
+                cursor: pointer;
+                background-color: ${ colours.grey5 };
+                color: ${ colours.grey5 };
+            }
+            :active {
+                cursor: pointer;
+                background-color: ${ colours.grey6 };
+                color: ${ colours.grey6 };
+            }
+        `
+    }
+};
+
+export const Button = styled.button<ButtonStyleProps>`
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: ${ spacings._1 };
+    text-align: center;
+    font-weight: ${ fontWeights.bold };
+    transition: background-color 0.3s;
+    opacity: ${ p => p.disabled ? '0.6' : 'unset' };
+    pointer-events: ${ p => p.disabled ? 'none' : 'unset' };
+    padding: ${ spacings._4 } ${ spacings._6 };
+    border: none;
+
+    ${ p => p.profile && profileColourSettings( p.profile ) }
+    :focus {
+        outline: none;
+    }
+
+    ${ p => p.size === 'small' && css`
+        padding: ${ spacings._1 } ${ spacings._3 };
+        font-size: ${ fontSizes.secondary };
+    ` }
+
+    ${ p => p.margins && margins( p.margins ) }
+`;
