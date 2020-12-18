c4267d3e8268b8c17a0813a7e4437b16c1bf4a65
Joan Samaras <jsam.art14@gmail.com>
2020-12-18 23:02:22 +0200

    feature/100 - removed console.log and performed review fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { colours } from 'design-system/colours';
 import { Step2 } from 'pages/Step2';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Wrapper = styled.div`
     display: flex;
